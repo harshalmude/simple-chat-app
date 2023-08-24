1ef26ed236ecc91892cb2c22f057234bf8c2b074
harshalmude <142514855+harshalmude@users.noreply.github.com>
2023-08-24 19:59:33 +0530

    app.js

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
   console.log('A user connected');
